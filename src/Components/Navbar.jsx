46fbdab132bc1eff1bd3223f2c2b5638a5cfd59f
Shimaa Mahmoud <shimaahriaz@gmail.com>
2024-01-03 07:09:11 +0200

    Finished SearchUsers

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,15 +1,19 @@
 import React from 'react';
 import {signOut} from "firebase/auth";
 import { auth } from '../firebase';
+import { useContext } from 'react';
+import { AuthContext } from './../context/AuthContext';
 
 
 const Navbar = () => {
+  const { currentUser } = useContext(AuthContext);
+
     return (
         <div className='navbar'>
         <span className="logo">Lama Chat</span>
         <div className="user">
-          <img src='https://media.istockphoto.com/id/482189340/photo/portrait-of-girl.jpg?s=612x612&w=0&k=20&c=-yarEEKOPTP33MVl8c8FRuTvOt73XNW17WBCLIhweiw=' alt="" />
-          <span>Johan</span>
+          <img src= {currentUser.photoURL} alt="" />
+          <span>{currentUser.displayName}</span>
           <button onClick={()=>signOut(auth)}>logout</button>
         </div>
       </div>    
